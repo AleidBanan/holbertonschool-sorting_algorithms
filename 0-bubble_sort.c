20adbfd3ff55c684df7f77e4977e29427fa03776
AleidBanan <Aleidbanan@gmail.com>
2025-10-23 15:12:59 +0000

    sort array task 0 sorting

---
+++ 0-bubble_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ * @array: pointer to the array to sort
+ * @size: number of elements in the array
+ *
+ * Return: nothing (void)
+ */
+
+void bubble_sort(int *array, size_t size)
+
+{
+	int count;
+
+	int i;
+
+	int temp;
+
+	int swapped;
+
+	if (array == NULL || size == 1)
+		return;
+
+
+	for (count = 0; count < size; count++)
+	{
+		int swapped = 0;
+
+		for (i = 0; i < size - count - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
+
+				swapped = 1;
+			}
+		}
+
+		if (swapped == 0)
+			break;
+	}
+}
